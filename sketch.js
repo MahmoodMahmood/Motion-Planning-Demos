89757a955219d48eddffcd6e7f75d894cc7b2d6f
Mahmood Mahmood <mahmood.mahmood@maymobility.com>
2020-12-29 16:15:00 -0500

    added rubberband optimization algorithm

--- sketch.js
+++ sketch.js
@@ -1,3 +1,22 @@
+class PointRobot {
+  constructor(state) {
+    this.x = state.x
+    this.y = state.y
+  }
+
+  stepToward(target, step_size) {
+    let diff_x = target.x - this.x
+    let diff_y = target.y - this.y
+    let mag = Math.sqrt(diff_x ** 2 + diff_y ** 2)
+    this.x = this.x + (step_size / mag) * diff_x
+    this.y = this.y + (step_size / mag) * diff_y
+  }
+
+  inCollision(obstacles) {
+    return obstacles.some(o => o.inObstacle(this.x, this.y));
+  }
+}
+
 class Node {
   constructor(x, y, parent) {
     this.x = x
@@ -25,10 +44,12 @@ class Node {
     while (path[path.length - 1].parent) {
       path.push(path[path.length - 1].parent)
     }
-    return path
+    return path.reverse()
   }
 
   draw() {
+    strokeWeight(1)
+    stroke('black')
     fill(this.color)
     circle(this.x, this.y, 10)
   }
@@ -36,6 +57,8 @@ class Node {
   drawRecursive() {
     this.draw()
     this.children.forEach(c => {
+      strokeWeight(1)
+      stroke('black')
       line(this.x, this.y, c.x, c.y)
       c.drawRecursive()
     })
@@ -43,11 +66,11 @@ class Node {
 }
 
 class Tree {
-  constructor(root, canvas_width, canvas_height, cellPointLimit) {
+  constructor(root, canvas_width, canvas_height, cell_point_limit, step_size) {
     this.root = root
     this.boundary = new Rectangle(canvas_width / 2, canvas_height / 2, canvas_width / 2, canvas_height / 2)
-    this.cellPointLimit = cellPointLimit
-    this.qtree = new Quad(this.boundary, this.cellPointLimit)
+    this.qtree = new Quad(this.boundary, cell_point_limit)
+    this.step_size = step_size
   }
 
   getAllNodes() {
@@ -72,29 +95,30 @@ class Tree {
     return this.root
   }
 
-  addNode(obstacles) {
-    while (1) { // keep looping until we find a valid node to add
-      const moveDist = 20
-      let x = Math.random() * 400
-      let y = Math.random() * 400
+  addNode(target, obstacles) {
+    let tries = 0;
+    while (++tries < 100) { // keep looping until we find a valid node to add
+      let sampleTgt = Math.random() < 0.1 // 10% chance to sample the target point
+      let x = sampleTgt ? target.x : Math.random() * 400
+      let y = sampleTgt ? target.y : Math.random() * 400
+
       // TEMP_NODE = new Node(x, y, null) // for visualization purposes
       // TEMP_NODE.color = 'red'
 
       // Core of RRT algorithm, see paper for details
-      let nearestNode = this.findNearestNode(x, y)
-      let diff_x = x - nearestNode.x
-      let diff_y = y - nearestNode.y
-      let mag = Math.sqrt(diff_x ** 2 + diff_y ** 2)
-
-      let newNode = new Node(nearestNode.x + (moveDist / mag) * diff_x, nearestNode.y + (moveDist / mag) * diff_y, nearestNode)
+      let nearest_node = this.findNearestNode(x, y)
+      let robot = new PointRobot(nearest_node)
+      robot.stepToward({x: x, y: y}, this.step_size)
 
       // make sure the new node is not in any obstacle, if it is then we retry everything
-      if (obstacles.every(o => !o.inObstacle(newNode.x, newNode.y))) {
-        nearestNode.addChild(newNode)
-        this.qtree.insert(newNode);
-        return newNode
+      if (!robot.inCollision(obstacles)) {
+        let new_node = new Node(robot.x, robot.y, nearest_node)
+        nearest_node.addChild(new_node)
+        this.qtree.insert(new_node);
+        return new_node
       }
-    }
+    } 
+    alert("failed to add node!")
   }
 
   findTarget(target, obstacles, maxNodes = 10, acceptable_range = 20) {
@@ -104,7 +128,7 @@ class Tree {
     }
 
     for (let i = 0; i < maxNodes; i++) {
-      let newNode = this.addNode(obstacles)
+      let newNode = this.addNode(target, obstacles)
       if (newNode.dist(target.x, target.y) < acceptable_range) {
         return newNode.getPathFromRoot()
       }
@@ -133,27 +157,71 @@ class Obstacle {
   }
 
   draw() {
+    strokeWeight(1)
+    stroke('black')
     rectMode(CORNERS)
     fill(this.color)
     rect(this.x1, this.y1, this.x2, this.y2)
   }
 }
 
+/*
+ *  <Path optimization functions>
+ */
+function rubberBandPass(path, obstacles, step_size, max_steps=10000) {
+  let new_path = [path[0]]
+  for (let i = 1; i < path.length - 1; i++) {
+    let robot = new PointRobot(new_path[new_path.length-1])
+    let done = false
+    for (let j = 0; !done && j < max_steps; j++) {
+      robot.stepToward(path[i+1], step_size)
+      // if robot collides then point at path[i] cannot be optimized out
+      if (robot.inCollision(obstacles)) {
+        new_path.push(path[i])
+        done = true 
+      } else if (path[i+1].dist(robot.x, robot.y) < 5) {
+        // if we arrived at path[i+1] without a collision then we don't need path[i]
+        done = true
+      }
+    }
+    // if (!done) {
+    //   // unable to reach next node for some reason, add path[i] to be safe
+    //   new_path.push(path[i])
+    // }
+  }
+  // always need to keep the last point
+  new_path.push(path[path.length-1])
+  return new_path
+}
+
+function optimizePath(path, obstacles, step_size) {
+  if (!path) return []
+  return rubberBandPass(path, obstacles, step_size)
+}
+
+/*
+ *  </Path optimization functions>
+ */
+
+
 const canvas_width = 400
 const canvas_height = 400
+const tree_step_size = 15
+const cell_point_limit = 6
+
 let TEMP_NODE = null // just an extra node we will draw in red for debugging purposes
 let target = new Node(350, 350, null)
 target.color = 'green'
-let targetReposition = false // when true, use is clicking and dragging target node
+let targetReposition = false // when true, user is clicking and dragging target node
 let rootReposition = false // when true, user is clicking and dragging root node
-const cellPointLimit = 6
 
 let root = new Node(100, 200, null)
 root.color = 'orange'
-let t = new Tree(root, canvas_width, canvas_height, cellPointLimit)
+let t = new Tree(root, canvas_width, canvas_height, cell_point_limit, tree_step_size)
 
 let obstacles = [] // list of obstacles to avoid
 let path = [] // path from root to target
+let optimized_path = [] // optimized path from root to target
 let clickLoc = null
 
 function setup() {
@@ -200,7 +268,8 @@ function draw() {
   if (TEMP_NODE) {
     TEMP_NODE.draw()
   }
-  if (path) path.forEach(p => p.draw())
+  if (path) drawPath(path, point_color='orange', line_color='#CC7000', line_width=2)
+  if (optimized_path) drawPath(optimized_path, point_color='purple', line_color='red', line_width=4)
 }
 
 function canvasMousePressed() {
@@ -218,6 +287,7 @@ function canvasMousePressed() {
   t = null
   root.children = []
   path = []
+  optimized_path = []
   // return false to prevent default
   return false
 }
@@ -236,8 +306,28 @@ function canvasMouseReleased() {
 
 function addPoint(pts) {
   if (!t) {
-    t = new Tree(root, canvas_width, canvas_height, cellPointLimit)
+    t = new Tree(root, canvas_width, canvas_height, cell_point_limit, tree_step_size)
   }
   path = t.findTarget(target, obstacles, maxNodes = pts, acceptable_range = 10)
   if (path) path.forEach(n => n.color = 'orange')
+}
+
+function drawPath(path_to_draw, point_color, line_color, line_width) {
+  for (let i = 0; i < path_to_draw.length; i++) {
+    let p = path_to_draw[i]
+    p.color = point_color
+    if (i > 0) {
+      strokeWeight(line_width)
+      stroke(line_color)
+      line(path_to_draw[i-1].x, path_to_draw[i-1].y, p.x, p.y)
+    }
+  }
+}
+
+function optimize() {
+  if (optimized_path.length > 0) {
+    optimized_path = optimizePath(optimized_path, obstacles, tree_step_size/5)
+  } else if (path.length > 0) {
+    optimized_path = optimizePath(path, obstacles, tree_step_size/5)
+  }
 }
