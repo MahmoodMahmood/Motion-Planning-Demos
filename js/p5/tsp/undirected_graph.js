7680b4a6ab9f92dc7b9c27fc92b2901a47ef49aa
Mahmood Mahmood <mahmood.a.yahya@gmail.com>
2022-09-05 02:36:17 +0400

    Added distances visualization between nodes

--- js/p5/tsp/undirected_graph.js
+++ js/p5/tsp/undirected_graph.js
@@ -36,6 +36,23 @@ function calcDist(node1, node2) {
 //
 function drawEdge(node1, node2) {
   line(node1.x, node1.y, node2.x, node2.y)
+
+  const angle = Math.atan2(node2.y-node1.y, node2.x-node1.x)
+  
+  // Prevents double drawing of distance text, only keep the right side up text
+  if (angle > Math.PI/2 || angle < -Math.PI/2) return
+  const dist = calcDist(node1, node2)
+  const midx = (node1.x + node2.x)/2
+  const midy = (node1.y + node2.y)/2
+  fill('black')
+  textSize(canvas_width/40)
+  
+  push()
+  translate()
+  translate(midx, midy)
+  rotate(angle)
+  text(dist.toPrecision(4), 0, 0)
+  pop()
 }
 
 function drawNode(node, config) {
